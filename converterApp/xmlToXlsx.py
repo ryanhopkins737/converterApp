2ff21f38f630e8447eb4dafb3dad6c01562fde8f
ifnameismain <75719905+ifnameismain@users.noreply.github.com>
2022-02-16 14:02:26 +0800

    Update xmlToXlsx.py

--- converterApp/xmlToXlsx.py
+++ converterApp/xmlToXlsx.py
@@ -1,57 +1,37 @@
 import xml.etree.ElementTree as ET
-from lxml import etree
-import xlsxwriter
+import pandas as pd
 
 
+def xml_to_df(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+    columns = set()
+    columns.add('node depth')
+    columns = recurse_columns(root, columns)
+    df = pd.DataFrame(columns=list(columns))
+    df = recurse_node(root, df)
+    print(df.head())
 
 
-tree = ET.parse(r'C:\Users\ryan.hopkins\Desktop\testPRM0356Params_MaintenanceInfo_20220111.xml')
-rootTag = tree.getroot()
-print(rootTag)
+def recurse_columns(node, columns: set, depth=0):
+    depth += 1
+    for child in node:
+        if depth > 1:
+            columns.add(child.tag)
+        _ = recurse_columns(child, columns, depth)
+    return columns
 
 
-allTags = set()
-# Adds all distinct tag names to a set from the XML file
-for x in rootTag.iter("*"):
-    allTags.add(x.tag)
-allTags = list(allTags)
+def recurse_node(node, df: pd.DataFrame, depth=0):
+    depth += 1
+    for child in node:
+        l = [child.text if child.tag == col else "" for col in df.columns]
+        l[-1] = depth
+
+        df.loc[df.shape[0]] = l
+        _ = recurse_node(child, df, depth)
+    return df
 
 
-# Creates a list with ordered tags and removes ones that are used
-orderedTags = [rootTag.tag]
-parent = [rootTag.tag]
-master = [rootTag.tag]
-next_parent = []
-
-findingTags = True
-
-# while findingTags:
-#     print('hi')
-#     for b in parent:
-#         for a in allTags:
-#             if b.find(a) == 0:
-#                 next_parent.append(a)
-#                 master.append(a)
-#                 print('hihi')
-#     if len(master) == len(allTags):
-#         findingTags = False
-#     print('bye')
-#     parent = next_parent
-#     next_parent = []
-
-# for y in rootTag.itertext():
-#     print(y)
-
-for z in rootTag.iter('*'):
-    print(f'{z.text}, {z.tag}')
-    text = z.text
-    tag = z.tag
-
-
-print(f'{rootTag.itertext()}')
-print(master)
-print(parent)
-print(next_parent)
-
-a = rootTag.findall('ParameterValueSetList')
-print(allTags)
+if __name__ == '__main__':
+    xml_to_df('plant_catalog.xml')
